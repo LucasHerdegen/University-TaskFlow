abb28930ba70125dc5a2c2e511f079e4cb005635
LucasHerdegen <lherdegen@frba.utn.edu.ar>
2026-04-06 01:07:54 -0300

    fix?

--- audit-service/src/main/java/com/taskflow/auditservice/services/AuditLogService.java
+++ audit-service/src/main/java/com/taskflow/auditservice/services/AuditLogService.java
@@ -3,6 +3,7 @@ package com.taskflow.auditservice.services;
 import com.taskflow.auditservice.entities.AuditLog;
 import com.taskflow.auditservice.repositories.AuditLogRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.rabbit.annotation.Queue;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 
@@ -14,10 +15,12 @@ public class AuditLogService
 {
     private final AuditLogRepository repository;
 
-    @RabbitListener(queues = "task.created.queue")
+    @RabbitListener(queuesToDeclare = @Queue("task.created.queue"))
     public void manageTaskCreation(String message)
     {
         var auditLog = new AuditLog(message, LocalDate.now());
         repository.save(auditLog);
+
+        System.out.println("Audit log saved in Mongo: " + message);
     }
 }
